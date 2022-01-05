de98304920e154b79c208ae7983477d8a3130f2f
SimonKib <simon@nosuchcompany.dk>
2022-01-05 13:34:01 +0100

    added dynamics to frontpage

---
+++ content/frontpage/main.md
@@ -0,0 +1,8 @@
+---
+createdAt: 2022-01-15
+title: It's No Such Company
+description: hey hey hey
+---
+## Frontpage liquentibus sinu verbis et spatiarer laedar
+
+Lorem markdownum viros voce nostri tumulandus contegat tua **prior** umbra
--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <section class="self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">Nuxt — Tailwind — Netlify CMS</h1>
       <h2 class="subtitle text-center">Boilerplate</h2>
+      <pre>{{content.description}}</pre>
     </section>
 
     <section class="mt-8">
@@ -11,3 +12,22 @@
     </section>
   </main>
 </template>
+<script>
+export default {
+  async asyncData({ $content, params, error }) {
+    let content;
+    try {
+      content = await $content("frontpage", "main").fetch();
+    } catch (e) {
+      error({ message: "Frontpage main content not found" });
+    }
+    return { content };
+  },
+  methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString)
+      return date.toLocaleDateString(process.env.lang) || ''
+    }
+  }
+}
+</script>
